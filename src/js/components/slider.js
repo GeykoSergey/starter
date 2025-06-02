9e06b2ff80a3440045d5f6ce25b56359c5dc2bc9
Geyko Sergey <sergeyko.dnepr@gmail.com>
2025-06-02 21:49:47 +0300

    first commit

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,7 +4,7 @@ import { Navigation, Scrollbar, Thumbs } from "swiper/modules";
 Swiper.use([Navigation, Scrollbar, Thumbs]);
 
 const newsSlider = document.querySelector(".news-slider");
-const productsSlider = document.querySelector(".products-slider");
+const testimonialsSlider = document.querySelector(".testimonials-slider");
 // const cardSlider = document.querySelector(".card-slider");
 
 new Swiper(newsSlider, {
@@ -18,63 +18,33 @@ new Swiper(newsSlider, {
     nextEl: ".news-slider__btn--next",
     prevEl: ".news-slider__btn--prev",
   },
-	// breakpoints: {
-	// 	640: {
-	// 		slidesPerView: 2,
-	// 		spaceBetween: 24,
-	// 	},
-	// 	768: {
-	// 		slidesPerView: 3,
-	// 		spaceBetween: 24,
-	// 	},
-	// 	1024: {
-	// 		slidesPerView: 4,
-	// 		spaceBetween: 24,
-	// 	},
-	// },
+  // breakpoints: {
+  // 	640: {
+  // 		slidesPerView: 2,
+  // 		spaceBetween: 24,
+  // 	},
+  // 	768: {
+  // 		slidesPerView: 3,
+  // 		spaceBetween: 24,
+  // 	},
+  // 	1024: {
+  // 		slidesPerView: 4,
+  // 		spaceBetween: 24,
+  // 	},
+  // },
 });
 
-// new Swiper(productsSlider, {
-//   slidesPerView: 4,
-//   spaceBetween: 40,
-// });
-
-// const slider = document.querySelector('.slider-main');
-// const sliderNav = document.querySelector('.slider-nav');
-
-// let mySwiperNav = new Swiper(sliderNav, {
-// 	slidesPerView: 6,
-// 	spaceBetween: 16,
-// 	loopedSlides: 5,
-// 	freeMode: true,
-// 	watchSlidesVisibility: true,
-// 	watchSlidesProgress: true,
-// 	loop: true,
-// 	direction: 'vertical',
-// 	pagination: {
-// 		el: '.swiper-pagination',
-// 		clickable: true,
-// 	},
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// })
-
-// let mySwiper = new Swiper(slider, {
-// 	spaceBetween: 10,
-// 	loop: true,
-// 	loopedSlides: 5,
-// 	pagination: {
-// 		el: '.swiper-pagination',
-// 		clickable: true,
-// 	},
-// 	navigation: {
-// 		nextEl: '.swiper-button-next',
-// 		prevEl: '.swiper-button-prev',
-// 	},
-// 	thumbs: {
-// 		swiper: mySwiperNav,
-// 	}
-// })
-
+new Swiper(testimonialsSlider, {
+  spaceBetween: 24,
+  loop: true,
+  speed: 600,
+  autoplay: {
+    delay: 5000,
+  },
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
